ad4a7900903bbac6844030c482ff10181be1a14c
Neil Lambert <nlambert@doximity.com>
2023-04-19 09:06:05 -0700

    whichkey: add <leader>bF to grab current buffer filepath

--- mappings.lua
+++ mappings.lua
@@ -18,6 +18,12 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>bF"] = { 
+        function()
+          vim.cmd('let @+=expand("%:p")')
+        end,
+        desc = "Copy filepath to clipboard",
+    },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
