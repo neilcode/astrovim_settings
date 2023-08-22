a3307deed24b0e7e5c2cb5fb1971331387938842
Neil Lambert <nlambert@doximity.com>
2023-08-22 15:25:12 -0700

    add projectMgr

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,22 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.editing-support.nvim-treesitter-endwise"}
+  { import = "astrocommunity.editing-support.nvim-treesitter-endwise"},
+  { import = "astrocommunity.project.projectmgr-nvim"},
+  { "charludo/projectmgr.nvim", 
+    opts = {
+      autogit = {
+        enabled = false,
+        command = "git pull --ff-only",
+      },
+      reopen = false,
+      session = { enabled = true, file = "Session.vim" },
+      shada = { enabled = false, file = "main.shada" },
+      scripts = {
+        enabled = true,
+        file_startup = "startup.sh",
+        file_shutdown = "shutdown.sh",
+      },
+    }
+  },
 }
