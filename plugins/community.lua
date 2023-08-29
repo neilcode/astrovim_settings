6d05816c657652cefcd051690807f1af669c0504
Neil Lambert <nlambert@doximity.com>
2023-08-29 12:55:42 -0700

    add smooth scroll and nvim-surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,9 +5,9 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.editing-support.nvim-treesitter-endwise"},
-  { import = "astrocommunity.project.projectmgr-nvim"},
-  { "charludo/projectmgr.nvim", 
+  { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
+  { import = "astrocommunity.project.projectmgr-nvim" },
+  { "charludo/projectmgr.nvim",
     opts = {
       autogit = {
         enabled = false,
@@ -17,10 +17,33 @@ return {
       session = { enabled = true, file = "Session.vim" },
       shada = { enabled = false, file = "main.shada" },
       scripts = {
-        enabled = true,
+        enabled = false,
         file_startup = "startup.sh",
         file_shutdown = "shutdown.sh",
       },
     }
   },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  { "declancm/cinnamon.nvim",
+    opts = {
+      -- KEYMAPS:
+      default_keymaps = true,   -- Create default keymaps.
+      extra_keymaps = true,    -- Create extra keymaps.
+      extended_keymaps = false, -- Create extended keymaps.
+      override_keymaps = false, -- The plugin keymaps will override any existing keymaps.
+
+      -- OPTIONS:
+      always_scroll = false,    -- Scroll the cursor even when the window hasn't scrolled.
+      centered = true,          -- Keep cursor centered in window when using window scrolling.
+      disabled = false,         -- Disables the plugin.
+      default_delay = 1,        -- The default delay (in ms) between each line when scrolling.
+      hide_cursor = true,      -- Hide the cursor while scrolling. Requires enabling termguicolors!
+      horizontal_scroll = true, -- Enable smooth horizontal scrolling when view shifts left or right.
+      max_length = -1,          -- Maximum length (in ms) of a command. The line delay will be
+                                -- re-calculated. Setting to -1 will disable this option.
+      scroll_limit = 100,       -- Max number of lines moved before scrolling is skipped. Setting
+                                -- to -1 will disable this option.
+    }
+  },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
